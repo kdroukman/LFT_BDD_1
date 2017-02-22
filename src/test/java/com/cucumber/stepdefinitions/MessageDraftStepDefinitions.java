a7f58285222f10b9f4d4d2b41d1455e510c87b30
Kate Droukman <kate.droukman@gmail.com>
2017-02-22 13:51:40 +1100

    US #28032 Continue Working on TDD

---
+++ src/test/java/com/cucumber/stepdefinitions/MessageDraftStepDefinitions.java
@@ -0,0 +1,37 @@
+package com.cucumber.stepdefinitions;
+
+import com.hp.lft.unittesting.UnitTestBase;
+import com.hp.lft.verifications.Verify;
+import cucumber.api.PendingException;
+import cucumber.api.java.Before;
+import cucumber.api.java.After;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import cucumber.api.Scenario;
+import com.hp.lft.sdk.*;
+import com.hp.lft.report.*;
+
+/**
+ * Created by droukman on 2/22/2017.
+ */
+public class MessageDraftStepDefinitions {
+
+    @Given("^I am on the contact us page$")
+    public void i_am_on_the_contact_us_page() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I enter (\\d+) characters$")
+    public void i_enter_characters(int arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^The full message is entered successfully$")
+    public void the_full_message_is_entered_successfully() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
--- src/test/java/com/cucumber/stepdefinitions/RunCukeTest.java
+++ src/test/java/com/cucumber/stepdefinitions/RunCukeTest.java
@@ -5,11 +5,12 @@ package com.cucumber.stepdefinitions;
  */
 
 import org.junit.runner.RunWith;
+import com.hpe.alm.octane.OctaneCucumber;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
-@RunWith(Cucumber.class)
+@RunWith(OctaneCucumber.class)
 @CucumberOptions(
         features = "src/test/java/com/cucumber/features",
         glue = "com.cucumber.stepdefinitions",
